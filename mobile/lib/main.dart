5c789080840a5c871ec1e9256ab4d64f9d289237
Umar Mahamud <umahamud9@gmail.com>
2026-03-24 21:32:34 +0300

    feat: Initialize dependency injection and sync service in main function

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -5,7 +5,17 @@ import 'core/routes/app_routes.dart';
 import 'features/inventory/presentation/manager/bloc/inventory_bloc.dart';
 import 'features/inventory/presentation/manager/bloc/inventory_event.dart';
 
-void main() {
+import 'package:mobile/injection_container.dart' as di;
+import 'package:mobile/shared/sync/sync_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await di.init();
+
+  final syncService = di.sl<SyncService>();
+  syncService.startListening();
+
   runApp(
     MultiBlocProvider(
       providers: [
