c9e38ae51784c65f0edd4dc82a291cfc65aad670
Mice <mwenyachileshem@gmail.com>
2026-03-25 13:24:57 +0200

    Deleted 'Expense' and moved everything to 'Expenses' combining all the final changes there.

--- mobile/lib/features/dashboard/presentation/pages/main_screen.dart
+++ mobile/lib/features/dashboard/presentation/pages/main_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dashboard_screen.dart';
 import '../../../sales/presentation/Pages/sales_screen.dart';
 import '../../../inventory/presentation/pages/inventory_page.dart';
-import '../../../expense/presentation/pages/expense_page.dart';
+import '../../../expenses/presentation/pages/expense_page.dart';
 import '../../../../core/widgets/custom_bottom_nav.dart';
 import '../../../settings/presentation/settings.dart';  
 // future: reports 
